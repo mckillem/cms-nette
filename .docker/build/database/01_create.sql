ccf0a5452a8110693428c8d03bcc77ef4c38f87a
Matěj <mckillem@tuta.io>
2023-12-24 08:48:44 +0100

    vytvoření db

--- .docker/build/database/01_create.sql
+++ .docker/build/database/01_create.sql
@@ -1,13 +1,16 @@
-CREATE TABLE `todo`
-(
-    `id`       INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(255) NOT NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS `article` (
+                                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                                         `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                         `has_picture` int(11) DEFAULT NULL,
+                                         `title` varchar(255) DEFAULT NULL,
+                                         `url` varchar(255) DEFAULT NULL,
+                                         `short_description` varchar(255) DEFAULT NULL,
+                                         `description` text,
+                                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 # login: test, heslo: test (hashed)
-INSERT INTO `todo` (`id`, `title`)
-VALUES (1, 'nakup'),
-       (2, 'čuč'),
-       (3, 'nauč'),
-       (4, 'prodej');
+INSERT INTO `article` (`id`, `date_add`, `has_picture`, `title`, `url`, `short_description`, `description`) VALUES
+                                                                                                                (1, '2020-03-15 11:54:20', 1, 'Základ PHP', '1-zaklad-php', 'Zde se naučíme základy PHP.', 'Co je to PHP? Jak je rozjet na localu? A na konec základní program Ahoj světe.'),
+                                                                                                                (2, '2020-03-16 11:05:31', 1, 'CSS Prakticky', '2-css-prakticky', 'Koukneme se na zoubek CSS.', '<p>Co je to CSS? Uděláme první kroky a ovládneme jej. A na konec si ukážeme best-off neboli <strong>BootStrap</strong> <strong>framework</strong>.</p>');
