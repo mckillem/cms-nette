e42f99b6a0952341bae52eb85d6a76627c0116b3
Matěj <mckillem@tuta.io>
2024-08-02 15:04:20 +0200

    čistky

--- www/index.php
+++ www/index.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// inicializace prostředí + získání objektu Configurator
 $configurator = App\Bootstrap::boot();
+// vytvoření DI kontejneru
 $container = $configurator->createContainer();
+// DI kontejner vytvoří objekt Nette\Application\Application
 $application = $container->getByType(Nette\Application\Application::class);
+// spuštění Nette aplikace
 $application->run();
